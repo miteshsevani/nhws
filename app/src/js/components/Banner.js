9f7fb316d4705f4da0ea75dbb4669409985a53f5
miteshsevani <msevani@gmail.com>
2017-06-12 12:01:36 +0100

    Banner text styling

--- app/src/js/components/Banner.js
+++ app/src/js/components/Banner.js
@@ -7,7 +7,7 @@ export default class Banner extends React.Component {
             <section className="banner">
                 <div className="container">
                     <div className="row">
-                        <div className="col-sm-12 col-md-8">
+                        <div className="col-sm-12 col-md-10">
                             <div className="banner-content">
                                 <h2>{content}</h2>
                             </div>
