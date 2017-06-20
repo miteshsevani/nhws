b3fcdc4eaacab9d0e1a810e9588c5cd1e26abf0b
miteshsevani <msevani@gmail.com>
2017-06-20 23:10:59 +0100

    Added google analytics

--- app/src/js/components/Documentaries.js
+++ app/src/js/components/Documentaries.js
@@ -8,13 +8,15 @@ export default class Documentaries extends React.Component {
             <section id="documentaries">
                 <div className="container">
                     <div className="row">
-                        <div className="col-sm-12">
+                        <div className="col-md-12">
                             <h2>Documentaries</h2>
                         </div>
-                        <div className="col-sm-7">
+                    </div>
+                    <div className="row">
+                        <div className="col-md-12 col-lg-7">
                             <iframe width="100%" height="315" src="https://www.youtube-nocookie.com/embed/7hXTM7ehvtk?rel=0" frameBorder="0" allowFullScreen></iframe>
                         </div>
-                        <div className="col-sm-5">
+                        <div className="col-md-12 col-lg-5">
                             <h3>{content[0].title}: {content[0].subtitle}</h3>
                             <p>{content[0].description}</p>
                         </div>
