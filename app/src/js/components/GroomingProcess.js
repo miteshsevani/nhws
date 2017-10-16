77c43448829a579b8014fec2cdd430047437dc70
miteshsevani <msevani@gmail.com>
2017-10-16 12:09:22 +0100

    Added new videos

--- app/src/js/components/GroomingProcess.js
+++ app/src/js/components/GroomingProcess.js
@@ -4,19 +4,26 @@ import { Grid,Row,Col } from 'react-bootstrap';
 export default class GroomingProcess extends React.Component {    
     render() {
         const {content} = this.props;
-        return(
-            
+        return(            
             <section className="grooming-process">
                 <Grid>                    
-                    <Row className="show-grid">
-                        <Col sm={7}>
-                            <iframe width="100%" height="315" src={content[0].link} frameBorder="0" allowFullScreen></iframe>
-                        </Col>
-                        <Col sm={5}>
-                            <h2>{content[0].title}</h2>
-                            <p>{content[0].text}</p>
-                        </Col>
-                    </Row>
+                {content.map((video, key) => {
+                    return(    
+                        <Row key={key} className="show-grid">
+                            <Col sm={6}>
+                                <iframe width="100%" height="250" src={video.link} frameBorder="0" allowFullScreen></iframe>
+                            </Col>
+                            <Col sm={6}>
+                                <h3>{video.title}</h3>
+                                {video.text.map((paragraph, key) => {
+                                    return(
+                                        <p key={key}>{paragraph}</p>
+                                    )
+                                })}                                
+                            </Col>
+                        </Row>
+                    )
+                })}
                 </Grid>
             </section>
         )
